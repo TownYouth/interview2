d8471e450fd6682aa8a245ab905db574baec84ed
townYouth <1160235500@qq.com>
2021-05-14 14:45:47 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/css/global.css'
+import 'ant-design-vue/dist/antd.css';
+import ant from 'ant-design-vue'
 
 Vue.config.productionTip = false
+Vue.use(ant);
 
 new Vue({
   router,
